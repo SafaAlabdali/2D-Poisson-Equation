efee113e48b8d4f4d134c9e421b2f4c0d9c6b55a
Safa's PC <Safa's PC@DESKTOP-P3OT4UA>
2020-06-22 11:18:17 +0400

    Solve function uses the schur complement to solve linear system by extracting the matrix A to four blocks matrices.

--- solve.m
+++ solve.m
@@ -1,10 +1,12 @@
+% This fuction solves linear system Av=r 
+% by Schur complement (basic)
 function v=solve (A,r)
 n=size(A);
 n=n(1);
 r1=r(1:n/2);
-r2=r(n/2+1:n);
+r2=r((n/2)+1:n);
 
-% extracting A
+%=================extracting A to four blocks =================
 A11=A(1:n/2,1:n/2);
 A12=A(1:n/2,(n/2)+1:n);
 A21=A((n/2)+1:n,1:n/2);
@@ -14,15 +16,20 @@ A22=A((n/2)+1:n,(n/2)+1:n);
 m=size(A22);
 m=m(1);
 A22_tild= zeros(m,m);
+%= 1) A22 itself
+A22_tild=A22;
 
-%A22_tild=A22;             %A22 itself
-%A22_tild=diag(diag(A22)); %diagonal of A22
-%A22_tild=eye(size(m));    %identity
+%= 2) diagonal of A22
+%A22_tild=diag(diag(A22)); 
 
-A22_tild(1:1+m:m*m) = diag(A22);   % tridiagonal
-A22_tild(m+1:1+m:m*m) = diag(A22,1);
+%= 3) identity
+%A22_tild=eye(size(m));   
 
-%=================compute r1_tild=========
+%= 4) tridiagonal
+%A22_tild(1:1+m:m*m) = diag(A22);   
+%A22_tild(m+1:1+m:m*m) = diag(A22,1);
+
+%=================compute r1_tild==========
 r1_tild=r1- A12 *(A22_tild \ r2 ) ;
 
 %================solve sv=r1_tild=============
